3c110f76ba18c22fcd666ff13204842c7f7b9b84
George Magdy <georgtriple@gmail.com>
2022-02-23 21:37:47 +0200

    Add decode_morse_code function

--- morse-decode.rb
+++ morse-decode.rb
@@ -5,3 +5,13 @@ chars_to_morse = {
   "..."=>"S", "-"=>"T", "..-"=>"U", "...-"=>"V", ".--"=>"W", "-..-"=>"X",
   "-.--"=>"Y", "--.."=>"Z", "   "=>" ", " "=>""
 }
+
+def decode_morse_code(str)
+  result = ''
+  str = str.split(/   /)
+  str.each { |word|
+    word.split(/ /).each{ |char| result += chars_to_morse[char] }
+    result += ' '
+  }
+  return result
+end
